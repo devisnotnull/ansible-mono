985932edd785dff101e612bf70ee09c3324d646b
Alex Brown <alex@devnotnull.com>
2021-12-30 17:18:56 +0000

    Updated helm charts

--- scripts/route53.sh
+++ scripts/route53.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 AWS_ROUTE53_ZONEID="Z03501612HD7E79ETXM8B"
-HOSTNAME="test.devnotnull.com"
+HOSTNAME="api.devnotnull.com"
 TTL="600"
-IP=`curl http://ifconfig.co/ 2>/dev/null`
+IP="86.9.171.110"
 
 aws route53 change-resource-record-sets --hosted-zone-id $AWS_ROUTE53_ZONEID --change-batch "{ \"Changes\": [ { \"Action\": \"UPSERT\", \"ResourceRecordSet\": { \"Name\": \"$HOSTNAME\", \"Type\": \"A\", \"TTL\": $TTL, \"ResourceRecords\": [ { \"Value\": \"$IP\" } ] } } ] }"
 echo "Updated the DNS Zone to $IP"
